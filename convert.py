da36ebcac6f570119d31ca1df53925fcdb6b7950
Maks Danylchuk <maxitosm00@gmail.com>
2023-02-22 13:00:03 +0200

    Added os.getcwd() which returns current working directory of a process, created new directory xls, added a prints of start and ending of process

--- convert.py
+++ convert.py
@@ -4,16 +4,19 @@ import glob
 
 
 def convert_xls_to_xlsx():
+    print("Started convert...")
     o = win32com.client.Dispatch("Excel.Application")
     o.Visible = False
-    input_dir = r"D:\Test Project Dexima\domestic_zones"
-    output_dir = r"D:\Test Project Dexima\domestic_zones\xlsx"
+    input_dir = f"{os.getcwd()}/xls"
+    output_dir = f"{os.getcwd()}/xlsx"
     files = glob.glob(input_dir + "/*.xls")
 
     for filename in files:
         file = os.path.basename(filename)
-        output = output_dir + '/' + file.replace('.xls', '.xlsx')
+        output = output_dir + "/" + file.replace(".xls", ".xlsx")
         wb = o.Workbooks.Open(filename)
         wb.ActiveSheet.SaveAs(output, 51)
         wb.Close(True)
-        os.remove(file)
+        os.remove(input_dir + "/" + file)
+
+    print("convert completed")
--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from convert import convert_xls_to_xlsx
 from create_an_excel import data_frame
 
 const_url = "https://www.ups.com/media/us/currentrates/zone-csv/"
+print("started parsing...")
 for i in range(5, 995):
     if i < 10:
         url = f"{const_url}00{i}.xls"
         response = requests.get(url, allow_redirects=True)
 
-        if response.headers.get('content-type') == "application/vnd.ms-excel":
-            open(f'00{i}.xls', 'wb').write(response.content)
+        if response.headers.get("content-type") == "application/vnd.ms-excel":
+            open(f"xls/00{i}.xls", "wb").write(response.content)
             data_rows = [[f"00{i}00-00{i}99", f"00{i}00", f"00{i}99"]]
 
             for sheet in data_frame.worksheets:
@@ -20,8 +21,8 @@ for i in range(5, 995):
         url = f"{const_url}0{i}.xls"
         response = requests.get(url, allow_redirects=True)
 
-        if response.headers.get('content-type') == "application/vnd.ms-excel":
-            open(f'0{i}.xls', 'wb').write(response.content)
+        if response.headers.get("content-type") == "application/vnd.ms-excel":
+            open(f"xls/0{i}.xls", "wb").write(response.content)
             data_rows = [[f"0{i}00-0{i}99", f"0{i}00", f"0{i}99"]]
 
             for sheet in data_frame.worksheets:
@@ -32,8 +33,8 @@ for i in range(5, 995):
         url = f"{const_url}{i}.xls"
         response = requests.get(url, allow_redirects=True)
 
-        if response.headers.get('content-type') == "application/vnd.ms-excel":
-            open(f'{i}.xls', 'wb').write(response.content)
+        if response.headers.get("content-type") == "application/vnd.ms-excel":
+            open(f"xls/{i}.xls", "wb").write(response.content)
             data_rows = [[f"{i}00-{i}99", f"{i}00", f"{i}99"]]
 
             for sheet in data_frame.worksheets:
@@ -42,4 +43,5 @@ for i in range(5, 995):
 
         data_frame.save("Carriers zone ranges.xlsx")
 
+print("parsing completed")
 convert_xls_to_xlsx()
